0cfd429a8da36f0c2dc5149d8497a05f034c2f44
Gachay <qacayjabrayilov@gmail.com>
2024-11-29 12:24:47 +0400

    first commit

---
+++ oop/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace StudentProfessorApp
+{
+    
+    class StudentProfessorTest
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Greet();
+
+            Student student = new Student();
+            student.SetAge(21);
+            student.Greet();
+            student.ShowAge();
+            student.Study();
+
+            Professor professor = new Professor();
+            professor.SetAge(40);
+            professor.Greet();
+            professor.Explain();
+
+        }
+    }
+
+    class Person
+    {
+        public int age;
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+    }
+    class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'am studying");
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is " + age + " years old");
+        }
+
+    }
+    class Professor : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'am explaining");
+        }
+    
+    }
+
+
+
+}
---
+++ oop/Program.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//namespace MyOpp
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Person person1 = new Person("Juan");
+//            Person person2 = new Person("Sara");
+//            Person person3 = new Person("Carlos");
+
+//            Console.WriteLine(person1.ToString());
+//            Console.WriteLine(person2.ToString());
+//            Console.WriteLine(person3.ToString());
+
+//        }
+//    }
+
+//    class Base
+//    {
+//        public virtual string Hello()
+//        {
+//            Console.WriteLine("hello");
+//            return "hello";
+//        }
+//    }
+//    class Person : Base
+//    {
+//        public String name;
+        
+//        public Person(String name)
+//        {
+//            this.name = name;
+//        }
+
+//        public override string ToString()
+//        {
+//            return "Hello! My name is" + " " + name;
+//        }
+
+//    }
+
+//}
